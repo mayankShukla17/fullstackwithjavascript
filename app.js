28ea9b1b0fc671f29da45d7567d7a348715ccb55
mayank <mayankgonda@gmail.com>
2019-10-02 16:13:06 +0530

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-alert('test');
+// alert('test');
+
+// var str="MKS";
+// var str1='mayank';
+// var str2=`MAYANK MKS`;
+
+// console.log(str + str1 + str2);
+
+// var phrase=`can ${str}`;
+
+// let name ="John";
+// alert('Hello, ${name}!');
+
+// alert('the result is ${1+2}');
+
+var name="MKS";
+var age=20;
+var company="GOOGLE";
+var salary=1000000000000000;
+
+console.log("My name is " + name + " my age is " + age + " I am working in " + company + " i am getting "+ salary);
+// ES5 way
+
+// console.log(`My name is ${name}.
+// My age is ${age}.
+// My company is ${company}.
+// My salary is ${salary}`);
+ document.write(`My name is ${name}.
+ My age is ${age}.
+ My company is ${company}.
+ My salary is ${salary}`);
+ 
