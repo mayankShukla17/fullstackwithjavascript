b16331446b7055d10d550d8eac74956a7cd64eea
mayank <mayankgonda@gmail.com>
2019-10-02 17:00:26 +0530

    commit

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // alert('test');
 
-// var str="MKS";
-// var str1='mayank';
-// var str2=`MAYANK MKS`;
+var str="MKS";
+var str1='mayank';
+var str2=`MAYANK MKS`;
 
 // console.log(str + str1 + str2);
 
@@ -30,3 +30,23 @@ console.log("My name is " + name + " my age is " + age + " I am working in " + c
  My company is ${company}.
  My salary is ${salary}`);
  
+
+
+ console.log(typeof name);
+console.log(typeof str1);
+console.log(typeof str2);
+
+// Number
+var num=10;
+var num1=10.10;
+var num2=1/1;
+
+console.log(typeof num);
+console.log(typeof num1);
+console.log(typeof num2);
+
+
+
+
+
+
